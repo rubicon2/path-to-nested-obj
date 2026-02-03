5185de5f07db403b3689e71016c3c9b9bc758c85
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-02-03 14:14:06 +0000

    Add jsdoc comments so declaration file can be generated

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * @param {string} path The path that describes the object shape. E.g. 'my/nested/key' which will produce { my: { nested: { key: value } } }
+ * @param {string} pathSeparator The character or string that determines how to split the path into object keys.
+ * @param {*} value The value to be placed at the deepest level of the resulting object. Can be any type, including null or undefined.
+ * @returns {object} An object matching the structure described by the path parameter, containing the value parameter at its deepest level.
+ */
 function pathToNestedObj(path, pathSeparator, value) {
   const nestedPath = {};
   const pathElements = path.split(pathSeparator);
